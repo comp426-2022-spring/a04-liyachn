ad61c0fcc479c40ae82e3b5f9d6fbb84f18cba6b
liyachn <liyachen@live.unc.edu>
2022-04-25 00:18:37 -0400

    database reference

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use( (req, res, next) => { // middleware function inserting new record in da
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
     }
-    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
     stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
     next()
 })
@@ -113,7 +113,7 @@ if (args.debug == true)
 {
     app.get('/app/log/access/', (req, res) => {
         app.get('/app/log/access', (req, res) => {
-            const stmt = db.prepare('SELECT * FROM accesslog').all() // log all data in accesslog
+            const stmt = logdb.prepare('SELECT * FROM accesslog').all() // log all data in accesslog
             res.status(200).json(stmt)
         });
     })
